dddf95e5623a61547719a6f2f1bbd5be30daa2cf
Kaung Myat <softcraftkmh@gmail.com>
2022-01-29 18:35:00 +0630

    fix: Duplicate Keys

--- components/scheduleGroup.tsx
+++ components/scheduleGroup.tsx
@@ -1,29 +1,29 @@
 import styles from "../styles/components/scheduleGroup.module.scss";
 
 type Schedule = {
-  name: string;
-  time: string;
+	name: string;
+	time: string;
 };
 
 type ScheduleGroupProps = {
-  title: string;
-  schedules: Array<Schedule>;
+	title: string;
+	schedules: Array<Schedule>;
 };
 
 const ScheduleGroup = (props: ScheduleGroupProps) => {
-  return (
-    <div>
-      <h2 className={styles.title}>{props.title}</h2>
-      <div className={styles.container}>
-        {props.schedules.map((schedule) => (
-          <div className={styles.item} key={schedule.name}>
-            <span>{schedule.name}</span>
-            <span>{schedule.time}</span>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<h2 className={styles.title}>{props.title}</h2>
+			<div className={styles.container}>
+				{props.schedules.map((schedule, i) => (
+					<div className={styles.item} key={i}>
+						<span>{schedule.name}</span>
+						<span>{schedule.time}</span>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default ScheduleGroup;
