58ebcead990ba170af43cca8cf69839a78407daa
Kaung Myat <softcraftkmh@gmail.com>
2022-02-05 21:21:08 +0630

    Editor UI

---
+++ components/input.tsx
@@ -0,0 +1,28 @@
+import { HTMLInputTypeAttribute } from "react";
+import { useField } from "formik";
+
+import styles from "../styles/components/input.module.scss";
+
+type InputProps = {
+	name: string;
+	label: string;
+	type?: HTMLInputTypeAttribute;
+};
+
+const Input = ({ name, label, type = "text" }: InputProps) => {
+	const [field, meta] = useField(name);
+
+	return (
+		<>
+			<div className={styles.container}>
+				{label && <label htmlFor={name}>{label}</label>}
+
+				<input type={type} id={name} className={styles.input} {...field} />
+
+				{<p className={styles.error}>{meta.touched && meta.error}</p>}
+			</div>
+		</>
+	);
+};
+
+export default Input;
--- pages/editor.tsx
+++ pages/editor.tsx
@@ -1,12 +1,27 @@
+import { FieldArray, Form, Formik } from "formik";
 import Head from "next/head";
-import { useState } from "react";
+import React, { useState } from "react";
 
 import Nav from "../components/nav";
+import WeeklySchedule from "../components/weeklySchedule";
 import styles from "../styles/pages/editor.module.scss";
 
 import type { ScheduleGroup } from "../models";
-import WeeklySchedule from "../components/weeklySchedule";
-import { Formik, Field, Form } from "formik";
+import Input from "../components/input";
+
+const EMPTY_SCHEDULE = {
+	name: "",
+	time: "",
+};
+
+const EMPTY_GROUP = {
+	title: "",
+	schedules: [
+		{
+			...EMPTY_SCHEDULE,
+		},
+	],
+};
 
 const Editor = () => {
 	const [scheduleGroups, setScheduleGroups] = useState<ScheduleGroup[]>([]);
@@ -14,13 +29,7 @@ const Editor = () => {
 	const initialValues = {
 		groups: [
 			{
-				title: "",
-				schedules: [
-					{
-						name: "",
-						time: "",
-					},
-				],
+				...EMPTY_GROUP,
 			},
 		],
 	};
@@ -34,20 +43,109 @@ const Editor = () => {
 			<div className={styles.container}>
 				<div>
 					<h2>Editor</h2>
-					<Formik initialValues={initialValues} onSubmit={console.log}>
-						{({ values, setFieldValue }) => (
+					<Formik
+						initialValues={initialValues}
+						onSubmit={(values) => {
+							setScheduleGroups(values.groups);
+						}}
+					>
+						{({ values, setFieldValue, submitForm, errors }) => (
 							<Form className={styles.form}>
-								{values.groups.map((foo, i) => (
-									<div key={i}>
-										<label htmlFor={`group.${i}.title`}>Title</label>
-										<Field
-											name={`group.${i}.title`}
-											placeholder="Sunday"
-											type="text"
-										/>
-									</div>
-								))}
-								<button type="submit">Submit</button>
+								<FieldArray name="groups">
+									{({
+										insert: insertGroup,
+										remove: removeGroup,
+										push: pushGroup,
+									}) => (
+										<div
+											style={{
+												border: "1px solid red",
+												padding: 12,
+												margin: 12,
+											}}
+										>
+											{values.groups.map((foo, groupIndex) => (
+												<div key={groupIndex}>
+													<Input
+														name={`groups.${groupIndex}.title`}
+														label="Day"
+													/>
+													{/* --> expected result : groups.1.schedules */}
+													{/* "groups." + groupIndex + ".schedules" */}
+													<FieldArray name={`groups.${groupIndex}.schedules`}>
+														{({
+															insert: insertSchedule,
+															remove: removeSchedule,
+															push: pushSchedule,
+														}) => (
+															<div
+																style={{
+																	border: "1px solid green",
+																	padding: 12,
+																	margin: 12,
+																}}
+															>
+																{values.groups[groupIndex].schedules.map(
+																	(foo, scheduleIndex) => (
+																		<div
+																			key={scheduleIndex}
+																			style={{
+																				border: "1px solid blue",
+																				padding: 12,
+																				margin: 12,
+																			}}
+																		>
+																			<Input
+																				name={`groups.${groupIndex}.schedules.${scheduleIndex}.name`}
+																				label="Name"
+																			/>
+																			<Input
+																				name={`groups.${groupIndex}.schedules.${scheduleIndex}.time`}
+																				label="Time"
+																			/>
+																			{scheduleIndex !== 0 && (
+																				<button
+																					type="button"
+																					onClick={() => {
+																						removeSchedule(scheduleIndex);
+																					}}
+																				>
+																					Remove schedule
+																				</button>
+																			)}
+																		</div>
+																	)
+																)}
+																<button
+																	onClick={() =>
+																		pushSchedule({ ...EMPTY_SCHEDULE })
+																	}
+																	type="button"
+																>
+																	Add schedule
+																</button>
+															</div>
+														)}
+													</FieldArray>
+													{groupIndex !== 0 && (
+														<button onClick={() => removeGroup(groupIndex)}>
+															Remove group
+														</button>
+													)}
+												</div>
+											))}
+											<button
+												onClick={() => pushGroup({ ...EMPTY_GROUP })}
+												type="button"
+											>
+												Add group
+											</button>
+										</div>
+									)}
+								</FieldArray>
+								<button type="submit" onClick={submitForm}>
+									Submit
+								</button>
 							</Form>
 						)}
 					</Formik>
