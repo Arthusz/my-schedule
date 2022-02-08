c801b33f54dbc3aa81dda81263ae2581a8ed277f
Kaung Myat <softcraftkmh@gmail.com>
2022-02-08 20:38:03 +0630

    Exercise Note

--- pages/state.tsx
+++ pages/state.tsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 const State = () => {
 	const [count, setCount] = useState(0);
 
+	// exercise: decrease count by 1
+	// count should not be less than 0
+
 	return (
 		<div id="container">
 			<h1>State Lesson</h1>
